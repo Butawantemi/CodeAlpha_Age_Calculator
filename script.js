763961da2090a6a1abcee5bbab639c76060c8331
Butawantemi <japhetbuta@gmail.com>
2024-08-28 18:27:13 +0300

    Check if the day, month and year are valid

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ document.getElementById('calculateBtn').addEventListener('click', function(){
     const day = parseInt(document.getElementById('day').value);
     const month = parseInt(document.getElementById('month').value);
     const year = parseInt(document.getElementById('year').value);
+
+    // Check if the day, month and year are valid
+    if (day < 1 || day > 31){
+        alert('Invalid day');
+        return;
+    }
+    if (month < 1 || month > 12) {
+        alert('Invalid month');
+        return;
+    }
     
 })
